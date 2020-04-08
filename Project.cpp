622317d5bce203fd8db54896691f34b8ba4e9233
Ponyapat <42263205+Ponyapat@users.noreply.github.com>
2020-04-08 14:07:22 +0700

    update

--- Project.cpp
+++ Project.cpp
@@ -1,10 +1,23 @@
-#include <string>
+
 #include <iostream>
 #include <iomanip>
+#include<ctime>
+#include <stdio.h>     
+#include <stdlib.h>     
+#include <time.h> 
+#include"bot.h"
+
 using namespace std;
 
 int main  () {
 	int choice;
+	Connect4 t;
+	string name;
+	string answer;
+	srand(time(0));
+	int ch = rand() % 2 + 1;
+	int check = 0;
+
 
 cout << "\n";
 cout << "\n";	
@@ -20,29 +33,65 @@ cout << setw(60) << "XOXOXOX" << endl;
 cout << setw(60) << "OXOXOXO" << endl;
 cout << "\n";
 
-
-
-	
-	while(choice != 3){
-		cout << setw(60) << "1. Player 1 vs Player 2" << endl;
+cout << setw(60) << "1. Player 1 vs Player 2" << endl;
 		cout << setw(60) << "2. Player vs AI" << endl;
 		cout << setw(60) << "3. Exit";
 		cout << "\n";
 		cout << "\n";
 		cout << setw(60) << "Input your choice : ";
 		cin >> choice;
-
-		if(choice == 1){
-			cout << "human" << endl;
-		}else if(choice == 2){
-			cout << "bot" << endl;
-		}else{
-			cout << "End";
-			}
-		}
 	
-	return 0;
+
+	
+	while (choice == 2){
+			cout << "bot" << endl;
+	
+	
+	cout << "Enter your name : ";
+	cin >> name;
+	
+		int table[6][7] = {};
+		do {
+			cout << " 1   2   3   4   5   6   7" << endl;
+
+			t.display();
+			int colum;
+			int check_choose = 0;
+			if (ch == 1) {
+				while (check_choose == 0) {
+					cout << name <<" Choose colum :";
+					cin >> colum;
+
+					if (colum < 1 || colum > 7){
+						cout << "Error, Choose again!!!" << endl;
+						check_choose = 0;
+					}else {
+						check_choose = 1;
+					}
+				}
+				t.player(colum);
+				ch++;
+			}else if(ch == 2) {
+				int n = rand() % 7 + 1;
+				colum = n;
+				cout << "AI choose colum: " << colum << endl;
+				t.computer(colum);
+				ch--;
+			}
+			
+			if (t.check_win() == true) {
+				check = 0;
+				t.display();
+			}else {
+				check = 1;	
+			}
+		}while (check == 1);
+		cout << "Do you want to play Connect 4 again? [y,n]: ";
+		cin >> answer;
+	}
+	
 }
+	
 	
 
 
--- bot.h
+++ bot.h
@@ -1,6 +1,38 @@
 //ai from youtube and google
 
-/*void player(int col) {
+#include<iostream>
+#include<string>
+#include<stdio.h>
+
+#ifndef board_h
+#define board_h
+
+
+using namespace std;
+
+class Connect4
+{
+public:
+	Connect4();
+	~Connect4();
+
+	void display() {
+		for (int i = 0; i < 6; i++) {
+			for (int j = 0; j < 7; j++) {
+				cout << "" << table[i][j];
+				if (j < 8) {
+					cout << "|";
+				}
+			}
+			if (i < 7) {
+				cout << endl << "---+---+---+---+---+---+---+" << endl;
+			}else {
+				cout << endl;
+			}
+		}
+	}
+
+void player(int col) {
 		table[keep[col - 1]][col - 1] = name;
 		keep[col - 1]--;
 	}
@@ -14,7 +46,7 @@
 	bool check_win() { // Check AI or Player win game
 		for (int i = 6; i > 0; i--) { 
 			for (int j = 0; j < 7; j++) {
-				if (table[i][j] == talbe[i][j + 1] && table[i][j + 1] == table[i][j + 2] && table[i][j + 2] == table[i][j + 3] && table[i][j] != "   ") {
+				if (table[i][j] == table[i][j + 1] && table[i][j + 1] == table[i][j + 2] && table[i][j + 2] == table[i][j + 3] && table[i][j] != "   ") {
 					if (table[i][j] == name) {
 						cout << name << " Win!!!" << endl;
 					}else if (table[i][j] == Computer) {
@@ -56,5 +88,27 @@ private:string table[6][7];
 		string name;
 		string Computer;
 		int keep[7] = { 5,5,5,5,5,5,5}; 
+		
 };
-*/
+
+
+
+Connect4 :: Connect4()
+{
+	for (int i = 0; i < 6; i++) {
+		for (int j = 0; j < 7; j++) {
+			table[i][j] = "   ";
+		}
+	}
+	name = " X ";
+	Computer = " O ";
+}
+
+Connect4 ::~Connect4()
+{
+}
+
+#endif
+
+
+

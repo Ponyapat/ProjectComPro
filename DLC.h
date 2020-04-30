f33926eb860e61de69a77c3a5e1987e4450515f4
Ponyapat <42263205+Ponyapat@users.noreply.github.com>
2020-04-30 19:26:48 +0700

    ...

--- DLC.h
+++ DLC.h
@@ -1,4 +1,3 @@
-//ai from youtube and google
 
 #include<iostream>
 #include<string>
@@ -16,69 +15,79 @@ public:
 	Connect4();
 	~Connect4();
 
-	void display() {
-		for (int i = 0; i < 6; i++) {
-			for (int j = 0; j < 7; j++) {
-				cout << "" << table[i][j];
-				if (j < 8) {
+	void display(int row ,int column) {
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < column; j++) {
+				cout << "" << table[row][column];
+				if (j < column) {
 					cout << "|";
 				}
 			}
-			if (i < 7) {
-				cout << endl << "---+---+---+---+---+---+---+" << endl;
-			}else {
+			if (i < row) {
+				cout << endl;
+				for (int i = 0; i < column; i++){
+					cout <<  "+---" ;
+				}
+				cout << endl;
+				
+			}
+			else {
 				cout << endl;
 			}
 		}
 	}
 
-void player1(int col) {
+	void player1(int col) {
 		table[keep[col - 1]][col - 1] = name1;
 		keep[col - 1]--;
 	}
 
-void computer(int col) {
+	void computer(int col) {
 		table[keep[col - 1]][col - 1] = Computer;
 		keep[col - 1]--;
 	}
 
-void player2(int col) {
-	table[keep[col - 1]][col - 1] = name2;
-	keep[col - 1]--;
-}
+	void player2(int col) {
+		table[keep[col - 1]][col - 1] = name2;
+		keep[col - 1]--;
+	}
 
 
 	bool check_win() { // Check AI or Player win game
-		for (int i = 6; i > 0; i--) { 
-			for (int j = 0; j < 7; j++) {
-				if (table[i][j] == table[i][j + 1] && table[i][j] == table[i][j + 2] && table[i][j] == table[i][j + 3] && table[i][j] != "   ") { //  vertical plane
+		for (int i = 6; i > n; i--) {
+			for (int j = m; j < 7; j++) {
+				if (table[i][j] != "   " && table[i][j] == table[i][j + 1] && table[i][j] == table[i][j + 2] && table[i][j] == table[i][j + 3]  ) { //  horizontal plane
 					if (table[i][j] == name1) {
 						cout << "Player Win!!!" << endl;
-					}else if (table[i][j] == Computer) {
+					}
+					else if (table[i][j] == Computer) {
 						cout << "AI Win!!!" << endl;
 					}
 					return true;
 				}
-				if (table[i][j] == table[i - 1][j] && table[i][j] == table[i - 2][j] && table[i][j] == table[i - 3][j] && table[i][j] != "   ") { //  horizontal plane
+				if (table[i][j] != "   " && table[i][j] == table[i - 1][j] && table[i][j] == table[i - 2][j] && table[i][j] == table[i - 3][j] ) {  //  vertical plane
 					if (table[i][j] == name1) {
 						cout << "Player Win!!!" << endl;
-					}else if (table[i][j] == Computer) {
+					}
+					else if (table[i][j] == Computer) {
 						cout << "AI Win!!!" << endl;
 					}
 					return true;
 				}
-				if (table[i][j] == table[i - 1][j + 1] && table[i][j] == table[i - 2][j + 2] && table[i][j] == table[i - 3][j + 3] && table[i][j] != "   ") { //  "Backslash" diag
+				if (table[i][j] != "   " && table[i][j] == table[i - 1][j + 1] && table[i][j] == table[i - 2][j + 2] && table[i][j] == table[i - 3][j + 3] ) { //  "Backslash" diag
 					if (table[i][j] == name1) {
 						cout << "Player Win!!!" << endl;
-					}else if (table[i][j] == Computer) {
+					}
+					else if (table[i][j] == Computer) {
 						cout << "AI Win!!!" << endl;
 					}
 					return true;
 				}
-				if (table[i][j] == table[i - 1][j - 1] && table[i][j] == table[i - 2][j - 2] && table[i][j] == table[i - 3][j - 3] && table[i][j] != "   ") { //  "Forward slash" diag
+				if (table[i][j] != "   " && table[i][j] == table[i - 1][j - 1] && table[i][j] == table[i - 2][j - 2] && table[i][j] == table[i - 3][j - 3]) { //  "Forward slash" diag
 					if (table[i][j] == name1) {
 						cout << "Player Win!!!" << endl;
-					}else if (table[i][j] == Computer) {
+					}
+					else if (table[i][j] == Computer) {
 						cout << "AI Win!!!" << endl;
 					}
 					return true;
@@ -89,9 +98,9 @@ void player2(int col) {
 
 
 	bool checkplayer_win() { // Check Who win game
-		for (int i = 6; i > 0; i--) {
-			for (int j = 0; j < 7; j++) {
-				if (table[i][j] == table[i][j + 1] && table[i][j + 1] == table[i][j + 2] && table[i][j + 2] == table[i][j + 3] && table[i][j] != "   ") { //  vertical plane
+		for (int i = 6; i > n; i--) {
+			for (int j = 0; j < m; j++) {
+				if (table[i][j] == table[i][j + 1] && table[i][j + 1] == table[i][j + 2] && table[i][j + 2] == table[i][j + 3] && table[i][j] != "   ") { //  horizontal plane
 					if (table[i][j] == name1) {
 						cout << "Player 1 Win!!!" << endl;
 					}
@@ -100,7 +109,7 @@ void player2(int col) {
 					}
 					return true;
 				}
-				if (table[i][j] == table[i - 1][j] && table[i - 1][j] == table[i - 2][j] && table[i - 2][j] == table[i - 3][j] && table[i][j] != "   ") { //  horizontal plane
+				if (table[i][j] == table[i - 1][j] && table[i - 1][j] == table[i - 2][j] && table[i - 2][j] == table[i - 3][j] && table[i][j] != "   ") { //  vertical plane
 					if (table[i][j] == name1) {
 						cout << "Player 1 Win!!!" << endl;
 					}
@@ -133,20 +142,21 @@ void player2(int col) {
 
 
 
-private:string table[6][7];
+private:string table[n][m];
 		string name1;
 		string Computer;
 		string name2;
-		int keep[7] = { 5,5,5,5,5,5,5}; 
-		
+		int n, m;
+		int keep[100] = {5,5,5,5,5,5,5,5,5,5};
+
 };
 
 
 
-Connect4 :: Connect4()
+Connect4::Connect4()
 {
-	for (int i = 0; i < 6; i++) {
-		for (int j = 0; j < 7; j++) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
 			table[i][j] = "   ";
 		}
 	}
@@ -163,4 +173,3 @@ Connect4 ::~Connect4()
 
 
 #pragma once
-
---
+++ project.cpp
@@ -0,0 +1,178 @@
+//https://cs.nyu.edu/courses/fall16/CSCI-UA.0101-009/notes/Lecture12.pdf
+#include"DLC.h"
+#include <iostream>
+#include <iomanip>
+#include<ctime>
+#include <stdio.h>     
+#include <stdlib.h>     
+#include <time.h> 
+
+
+using namespace std;
+
+int main() {
+	int choice;
+	Connect4 t;
+	string name1, name2;
+	int row,column;
+	srand(time(0));
+	int ch = rand() % 2 + 1;
+	int check = 0;
+	int n, m;
+	string table[n][m] = {} ;
+	
+
+
+	cout << "\n";
+	cout << "\n";
+	cout << setw(60) << "Welcome" << endl;
+	cout << setw(60) << "To" << endl;
+	cout << setw(60) << "CONNECT 4 Game" << endl;
+	cout << "\n";
+	cout << setw(60) << "XOXOXOX" << endl;
+	cout << setw(60) << "OXOXOXO" << endl;
+	cout << setw(60) << "XOXOXOX" << endl;
+	cout << setw(60) << "OXOXOXO" << endl;
+	cout << setw(60) << "XOXOXOX" << endl;
+	cout << setw(60) << "OXOXOXO" << endl;
+	cout << "\n";
+
+	cout << setw(60) << "1. Player 1 vs Player 2" << endl;
+	cout << setw(60) << "2. Player vs AI" << endl;
+	cout << setw(60) << "3. Exit";
+	cout << "\n";
+	cout << "\n";
+
+	cout << setw(60) << "Input your choice : ";
+	cin >> choice;
+
+
+	while (choice >= 3) {
+		cout << "Good Bye !?!" << endl;
+		system("pause");
+		break;
+	}
+
+
+
+
+	while (choice == 2) {
+		cout << "Input Row :";
+		cin >> row;
+		cout << "Input Column :";
+		cin >> column;
+		
+		
+		cout << "bot" << endl;
+		cout << "Enter your name : ";
+		cin >> name1;
+
+
+		do {
+			int table[6][7] = {};
+
+			t.display(row,column);
+			int colum;
+			int check_choose = 0;
+			if (ch == 1) {
+				while (check_choose == 0) {
+					cout << name1 << " Choose colum :";
+					cin >> colum;
+
+					if (colum < 1 || colum > 7) {
+						cout << "Error, Choose again!!!" << endl;
+						check_choose = 0;
+					}
+					else {
+						check_choose = 1;
+					}
+				}
+				t.player1(colum);
+				ch++;
+			}
+			else if (ch == 2) {
+				int n = rand() % 7 + 1;
+				colum = n;
+				cout << "AI choose colum: " << colum << endl;
+				t.computer(colum);
+				ch--;
+			}
+
+			if (t.check_win() == true) {
+				check = 0;
+				t.display(row,colum);
+			}
+			else {
+				check = 1;
+			}
+		} while (check == 1);
+		cout << "End Game" << endl;
+		system("pause");
+		return 0;
+	}
+
+
+	while (choice == 1) {
+		cout << "PVP" << endl;
+		cout << "Enter Player 1 Name : ";
+		cin >> name1;
+		cout << "Enter Player 2 Name : ";
+		cin >> name2;
+
+		int table[6][7] = {};
+		do {
+			cout << " 1   2   3   4   5   6   7" << endl;
+
+			t.display(row,column);
+			int colum;
+			int check_choose = 0;
+			if (ch == 1) {
+				while (check_choose == 0) {
+					cout << name1 << " Choose colum :";
+					cin >> colum;
+
+					if (colum < 1 || colum > 7) {
+						cout << "Error, Choose again!!!" << endl;
+						check_choose = 0;
+					}
+					else {
+						check_choose = 1;
+					}
+				}
+				t.player1(colum);
+				ch++;
+			}
+			else if (ch == 2) {
+				while (check_choose == 0) {
+					cout << name2 << " Choose colum :";
+					cin >> colum;
+
+					if (colum < 1 || colum > 7) {
+						cout << "Error, Choose again!!!" << endl;
+						check_choose = 0;
+					}
+					else {
+						check_choose = 1;
+					}
+				}
+				t.player2(colum);
+				ch--;
+			}
+
+			if (t.checkplayer_win() == true) {
+				check = 0;
+				t.display(row,colum);
+			}
+			else {
+				check = 1;
+			}
+		} while (check == 1);
+		cout << "End Game" << endl;
+		system("pause");
+		return 0;
+	}
+
+
+
+	return 0;
+}
